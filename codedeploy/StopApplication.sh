76ad0ae6971e20f911ca8601504ba94ade8425a9
Keyu Liu <keyuliu@KeyudeMacBook-Pro.local>
2021-03-18 12:21:15 -0400

    codeDeploy

--- codedeploy/StopApplication.sh
+++ codedeploy/StopApplication.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu
+rm CloudNativeWebApp-0.0.1-SNAPSHOT.jar
 sudo fuser -k 8080/tcp
 if [  -f "CloudNativeWebApp-0.0.1-SNAPSHOT.jar" ];then
 rm CloudNativeWebApp-0.0.1-SNAPSHOT.jar
--- scripts/StopApplication.sh
+++ scripts/StopApplication.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu
-sudo fuser -k 8080/tcp
-
 rm CloudNativeWebApp-0.0.1-SNAPSHOT.jar
+sudo fuser -k 8080/tcp
+if [  -f "CloudNativeWebApp-0.0.1-SNAPSHOT.jar" ];then
+rm CloudNativeWebApp-0.0.1-SNAPSHOT.jar
+fi
