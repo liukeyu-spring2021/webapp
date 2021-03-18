89d097ab850e385330b47fa583ece8d316707d7a
Keyu Liu <keyuliu@KeyudeMacBook-Pro.local>
2021-03-18 13:43:21 -0400

    CodeDeploy Test

--- CloudNativeWebApp/src/main/java/com/cloud/CloudNativeWebAppApplication.java
+++ CloudNativeWebApp/src/main/java/com/cloud/CloudNativeWebAppApplication.java
@@ -21,7 +21,8 @@ public class CloudNativeWebAppApplication {
 	public static void main(String[] args) throws IOException {
 		DataSourceConfig.username = "root";
 		DataSourceConfig.password = "liukeyu521";
-		DataSourceConfig.hostname = "csye6225-f20.cp8alz277zmq.us-east-1.rds.amazonaws.com";
+		DataSourceConfig.hostname = "csye6225-f20.cbcz4zpbrrbg.us-east-1.rds.amazonaws.com";
+		//csye6225-f20.cbcz4zpbrrbg.us-east-1.rds.amazonaws.com
 		FileStorageService.S3_BUCKET_NAME ="webapps32";
 		FileStorageService.region = Region.US_EAST_1;
 		SpringApplication.run(CloudNativeWebAppApplication.class, args);
