a05c22f31cb15e42898caac171afb48dbb7d0d49
Keyu Liu <keyuliu@KeyudeMacBook-Pro.local>
2021-03-18 00:30:08 -0400

    codeDeploy

--- scripts/StartApplication.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu
-setsid java -jar CloudNativeWebApp-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
--- scripts/StopApplication.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu
-sudo fuser -k 8080/tcp
-if [  -f "CloudNativeWebApp-0.0.1-SNAPSHOT.jar" ];then
-rm CloudNativeWebApp-0.0.1-SNAPSHOT.jar
-fi
