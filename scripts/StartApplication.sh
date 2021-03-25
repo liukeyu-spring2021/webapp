6dcd98e12645052588378848d5889f8fc86f7e80
Keyu Liu <keyuliu@KeyudeMacBook-Pro.local>
2021-03-25 16:56:45 -0400

    cloudwatch

--- scripts/StartApplication.sh
+++ scripts/StartApplication.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/home/ubuntu/cloudwatch-config.json \
+    -s
 setsid java -jar CloudNativeWebApp-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
